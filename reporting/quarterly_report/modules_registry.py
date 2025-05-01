a25bb970e5dd35db807e0b1549160448fd961c4f
vinciliba <vinciguerradaniele@hotmail.com>
2025-05-01 08:47:54 +0200

    1st commit 1/05/2025

---
+++ reporting/quarterly_report/modules_registry.py
@@ -0,0 +1,25 @@
+from reporting.quarterly_report.modules.budget import BudgetModule
+from reporting.quarterly_report.modules.granting import GrantsModule
+from reporting.quarterly_report.modules.recoveries import RevenueModule
+from reporting.quarterly_report.modules.auri import AuriModule
+from reporting.quarterly_report.modules.controls import ControlsModule
+from reporting.quarterly_report.modules.edes import EdesModule
+from reporting.quarterly_report.modules.gf import GFModule
+from reporting.quarterly_report.modules.invoices import InvoicesModule
+from reporting.quarterly_report.modules.kpis import KPIsModule
+from reporting.quarterly_report.modules.payments import PaymentsModule
+from reporting.quarterly_report.modules.amendments import AmendmentModule
+
+MODULES = {
+    "Budget": BudgetModule,
+    "Grants": GrantsModule,
+    "Revenue": RevenueModule,
+    "Auri": AuriModule,
+    "Controls": ControlsModule,
+    "Edes": EdesModule,
+    "GF": GFModule,
+    "Invoices": InvoicesModule,
+    "KPIs": KPIsModule,
+    "Payments": PaymentsModule,
+    "Amendments": AmendmentModule
+}
---
+++ reporting/registry.py
@@ -0,0 +1,7 @@
+# In reporting/registry.py
+from .quarterly_report.modules_registry import MODULES as QUARTERLY_MODULES  # Fix: Import from modules_registry
+
+REPORT_MODULES_REGISTRY = {
+    "Quarterly_Report": QUARTERLY_MODULES,
+    # Add more mappings here
+}
